a15817483d5ad0a951617f7bb2801d406683c320
alicek106 <alice_k106@naver.com>
2019-03-14 04:12:49 -0400

    Modify loader.py

--- loader.py
+++ loader.py
@@ -1,14 +1,16 @@
 import argparse
 import numpy as np
 import tensorflow as tf
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_container", type=int, required=True)
+    parser.add_argument("--output_path", type=str, required=True)
     args = parser.parse_args()
 
     (train_x, train_y), (test_x, test_y) = tf.keras.datasets.mnist.load_data()
     n_data = len(train_x) // args.n_container
     for name, i in enumerate(range(0, len(train_x), n_data)):
         start, end = i, i + n_data
-        np.savez(str(name), x=train_x[start:end], y=train_y[start:end])
+        np.savez(os.path.join(args.output_path, str(name)), x=train_x[start:end], y=train_y[start:end])
