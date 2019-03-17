0dd8b167a8e454e1e8f0416dc53f69c6b07ee149
graykode <nlkey2022@gmail.com>
2019-03-17 10:36:39 +0900

    changed to form action

--- server/server.py
+++ server/server.py
@@ -1,43 +1,26 @@
 # -*- coding: utf-8 -*-
-"""
-    using flask_dropzone, license: MIT
-    see LICENSE for more details. https://github.com/greyli/flask-dropzone
-"""
-import os
 import numpy as np
 import tensorflow as tf
 import PIL.Image as img
 from flask import Flask, render_template, request
-from flask_dropzone import Dropzone
-
-model = None
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__)
-app.config.update(
-    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
-    # Flask-Dropzone config:
-    DROPZONE_ALLOWED_FILE_TYPE='image',
-    DROPZONE_MAX_FILE_SIZE=3,
-    DROPZONE_MAX_FILES=30,
-)
-
-dropzone = Dropzone(app)
+from werkzeug import secure_filename
 
 model = tf.keras.models.load_model('model.h5', compile=False)
 model._make_predict_function()
 session = tf.keras.backend.get_session()
 
+app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+   return render_template('index.html')
 
-@app.route('/upload', methods=['POST', 'GET'])
+@app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
         f = request.files.get('file')
-        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
-        fname = os.path.join(app.config['UPLOADED_PATH'], f.filename)
+        fname = secure_filename(f.filename)
+        f.save(fname)
 
         data = np.array([np.array(img.open(fname).resize((28, 28), img.ANTIALIAS))])
         data = np.expand_dims(data, -1) / 255.0
@@ -46,7 +29,7 @@ def upload():
             predict = model.predict(data)
             predict = np.argmax(predict[0])
 
-        return str(predict)
+        return render_template('predict.html', predict=str(predict))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
