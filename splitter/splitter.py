f530bd3585ccdfc3b75f5f6eec28f49d205e670c
graykode <nlkey2022@gmail.com>
2019-03-17 09:55:28 +0900

    changing value naming more clearly

--- splitter/splitter.py
+++ splitter/splitter.py
@@ -6,16 +6,16 @@ import os
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_container", type=int, required=True)
-    parser.add_argument("--output_path", type=str, required=True)
+    parser.add_argument("--savedir", type=str, required=True)
 
     args = parser.parse_args()
 
     # Create output path if not exists
-    if not os.path.exists(args.output_path):
-        os.makedirs(args.output_path)
+    if not os.path.exists(args.savedir):
+        os.makedirs(args.savedir)
 
     (train_x, train_y), (test_x, test_y) = tf.keras.datasets.mnist.load_data()
     n_data = len(train_x) // args.n_container
     for name, i in enumerate(range(0, len(train_x), n_data)):
         start, end = i, i + n_data
-        np.savez(os.path.join(args.output_path, str(name)), x=train_x[start:end], y=train_y[start:end])
+        np.savez(os.path.join(args.savedir, str(name)), x=train_x[start:end], y=train_y[start:end])
